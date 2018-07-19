0916ed3054fc492125f2ad63083b2b7b27437b17
guanjhensu <guanjhensu@gmail.com>
2018-07-19 16:22:37 +0800

    fetch data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -24,7 +24,7 @@ export const startAddExpense = (expenseData = {}) => {
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
-})
+});
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
@@ -37,3 +37,24 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value')
+      .then((snapshot) => {
+        const expenses = [];
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
+        });
+        dispatch(setExpenses(expenses));
+      })
+  }
+}
